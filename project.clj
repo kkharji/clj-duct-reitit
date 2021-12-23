9f045f3fe10c5f1f18e17687a6715d860ec2a28b
tami5 <kkharji@protonmail.com>
2021-12-23 22:07:29 +0300

    chore(*): add project.clj

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject duct/module.reitit "0.0.1"
+  :description "Duct module and router for the reitit routing library"
+  :url "https://github.com/tami5/clj-duct-reitit"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [ring-logger         "1.0.1"]
+                 [metosin/reitit      "0.5.15"]
+                 [duct/core           "0.7.0"]
+                 [integrant           "0.7.0"]
+                 [medley              "1.0.0"]]
+  :plugins [[cider/cider-nrepl      "0.27.3"]]
+  :profiles {:repl
+             {:dependencies [[clj-http/clj-http "3.12.3"]
+                             [ring/ring-mock    "0.4.0"]]}})
