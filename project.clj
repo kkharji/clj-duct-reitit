9194331ff72392f69e99d8091ff5af75d88923e6
tami5 <kkharji@protonmail.com>
2021-12-24 03:22:55 +0300

    chore(*): add dev profile

--- project.clj
+++ project.clj
@@ -11,5 +11,7 @@
                  [medley              "1.0.0"]]
   :plugins [[cider/cider-nrepl      "0.27.3"]]
   :profiles {:repl
-             {:dependencies [[clj-http/clj-http "3.12.3"]
+             {:source-paths   ["dev/src"]
+              :resource-paths ["dev/resources"]
+              :dependencies [[clj-http/clj-http "3.12.3"]
                              [ring/ring-mock    "0.4.0"]]}})
