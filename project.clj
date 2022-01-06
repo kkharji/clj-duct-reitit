663a8de930f1322f80cce4320fff53f348947339
tami5 <kkharji@protonmail.com>
2022-01-06 13:36:26 +0300

    feat(dev): pretty print exceptions
    Wow I didn't except such an improvement

--- project.clj
+++ project.clj
@@ -13,11 +13,15 @@
   :profiles {:repl
              {:source-paths   ["dev/src"]
               :resource-paths ["dev/resources"]
+              :middleware [io.aviso.lein-pretty/inject]
               :dependencies [[clj-http/clj-http "3.12.3"]
                              [ring/ring-mock    "0.4.0"]
                              [tami5/clj-dev     "0.1.1"]
                              [duct/logger.timbre  "0.5.0"]
-                             [duct/module.logging "0.5.0"]]}}
+                             [duct/module.logging "0.5.0"]
+                             [io.aviso/pretty "1.0"]]}}
   :plugins [[cider/cider-nrepl      "0.27.3"]
-            [lein-shell             "0.5.0"]]
+            [lein-shell             "0.5.0"]
+            [io.aviso/pretty        "1.0"]]
+
   :aliases {"update-changelog" ["shell" "./bin/update-changelog"]})
