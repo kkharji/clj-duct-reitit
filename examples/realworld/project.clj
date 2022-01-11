a89ed997fec806c015870ff0a8282f2753777d10
tami5 <kkharji@protonmail.com>
2022-01-11 20:51:03 +0300

    fix(dev): Failed to load class "org.slf4j.impl.StaticLoggerBinder"

--- examples/realworld/project.clj
+++ examples/realworld/project.clj
@@ -6,6 +6,12 @@
                  [org.clojure/core.async "1.5.648"]
                  [duct/module.sql "0.6.1"]
                  [duct/module.reitit "0.3.1"]
+
+                 [duct/module.logging "0.5.0"]
+                 [log4j/log4j "1.2.17" :exclusions
+                  [javax.mail/mail javax.jms/jms com.sun.jmdk/jmxtools com.sun.jmx/jmxri]]
+                 [org.slf4j/slf4j-log4j12 "1.7.1"]
+
                  [duct/middleware.buddy "0.2.0"]
                  [buddy/buddy-hashers "1.8.1"]
                  [metosin/malli "0.7.5"]]
