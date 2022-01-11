bf334d5ab0eb480038afb14bc1e9b5d669b741b1
tami5 <kkharji@protonmail.com>
2022-01-11 21:27:48 +0300

    ref(logging): remove logging to file

--- examples/realworld/project.clj
+++ examples/realworld/project.clj
@@ -4,23 +4,24 @@
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/core.async "1.5.648"]
-                 [duct/module.sql "0.6.1"]
-                 [duct/module.reitit "0.3.1"]
 
+                 ;; Database
+                 [duct/module.sql "0.6.1"]
+
+                 ;; Logging
                  [duct/module.logging "0.5.0"]
-                 [log4j/log4j "1.2.17" :exclusions
-                  [javax.mail/mail javax.jms/jms com.sun.jmdk/jmxtools com.sun.jmx/jmxri]]
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail javax.jms/jms com.sun.jmdk/jmxtools com.sun.jmx/jmxri]]
                  [org.slf4j/slf4j-log4j12 "1.7.1"]
 
+                 [duct/module.reitit "0.3.1"]
                  [duct/middleware.buddy "0.2.0"]
                  [buddy/buddy-hashers "1.8.1"]
                  [metosin/malli "0.7.5"]]
 
-  :profiles {:repl {:source-paths ["dev"]}
-                    ;; :prep-tasks   ^:replace ["javac" "compile"]}
+  :profiles {:repl {:source-paths ["dev"]
+                    :prep-tasks   ^:replace ["javac" "compile"]}
              :uberjar {:aot :all}}
   :plugins [[duct/lein-duct "0.12.3"]
             [cider/cider-nrepl      "0.27.3"]]
-  ; :main ^:skip-aot conduit.main
-  :resource-paths ["resources" "target/resources"])
-  ; :prep-tasks     ["javac" "compile" ["run" ":duct/compiler"]])
+  :resource-paths ["resources" "target/resources"]
+  :prep-tasks     ["javac" "compile" ["run" ":duct/compiler"]])
