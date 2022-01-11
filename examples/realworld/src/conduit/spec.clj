4d02b2ac497c16b241831ae2ea1083c101e1859e
tami5 <kkharji@protonmail.com>
2022-01-11 13:45:41 +0300

    feat(spec): add missing request/response specs

--- examples/realworld/src/conduit/spec.clj
+++ examples/realworld/src/conduit/spec.clj
@@ -5,8 +5,7 @@
   (:require [malli.core :as m]
             [malli.registry :as mr]
             [malli.util :as mu]
-            [conduit.spec.util :refer [schemas-from-feilds optional-keys closed-schema]]
-            [duct.reitit.util :refer [spy]]))
+            [conduit.spec.util :refer [schemas-from-feilds optional-keys closed-schema]]))
 
 (def ^:private fields
   {;; User Schema
@@ -42,59 +41,66 @@
    :comment/created-at       :string
    :comment/updated-at       :string})
 
-(def ^:private schema
-  (let [{:keys [comment article profile user]} (schemas-from-feilds fields)]
-    {:schema/user    (-> user    optional-keys closed-schema)
-     :schema/comment (-> comment optional-keys closed-schema)
-     :schema/article (-> article optional-keys closed-schema)
-     :schema/profile (-> profile optional-keys closed-schema)}))
-
 (def ^:private actions
-  {:actions.user/update :schema/user
-   :actions.user/login    [:map
-                           [:email :user/email]
-                           [:password :user/password]]
-   :actions.user/register [:map
-                           [:email :user/email]
-                           [:username :user/username]
-                           [:password :user/password]]
-   :actions.article/create [:map
-                            [:body :article/body]
-                            [:description :article/description]
-                            [:title :article/title]
-                            [:tag-list {:optional true} :article/tag-list]]
-   :actions.article/update [:map
-                            [:title {:optional true} :article/title]
-                            [:body {:optional true} :article/body]
-                            [:description {:optional true} :article/description]]})
+  [;; User-Specific actions fields
+   #:actions.user
+    {:update    :schema/user
+     :login     [:map
+                 [:email :user/email]
+                 [:password :user/password]]
+     :register  [:map
+                 [:email :user/email]
+                 [:username :user/username]
+                 [:password :user/password]]}
+
+  ;; Article-Specific actions fields
+   #:actions.article
+    {:create  [:map
+               [:body :article/body]
+               [:description :article/description]
+               [:title :article/title]
+               [:tag-list {:optional true} :article/tag-list]]
+     :update  [:map
+               [:title {:optional true} :article/title]
+               [:body {:optional true} :article/body]
+               [:description {:optional true} :article/description]]}
+
+  ;; Comment-Specific actions fields
+   #:actions.comment
+    {:create [:map [:body :comment/body]]}])
 
 (def ^:private requests
-  {:request.user/register   [:map [:user :actions.user/register]]
-   :request.user/update     [:map [:user :actions.user/update]]
-   :request.user/login      [:map [:user :actions.user/login]]
-   :request.article/create  [:map [:article :actions.article/create]]
-   :request.article/update  [:map [:article :actions.article/update]]})
+  [;; User-Specific Requests
+   #:request.user
+    {:register   [:map [:user    :actions.user/register]]
+     :update     [:map [:user    :actions.user/update]]
+     :login      [:map [:user    :actions.user/login]]}
+
+   ;; Article-Specific Requests
+   #:request.article
+    {:create     [:map [:article :actions.article/create]]
+     :update     [:map [:article :actions.article/update]]}
+
+   ;; Comment-Specific Requests
+   #:request.comment
+    {:create    [:map [:comment :actions.comment/create]]}])
 
 (def ^:private responses
-  {:response/article       [:map [:article :schema/article]]
-   :response/articles      [:map [:articles [:vec :schema/article] [:count :int]]]
-   :response/user          [:map [:user :schema/user]]
-   :response/comment       [:map [:comment :schema/comment]]
-   :response/comments      [:map [:articles [:vec :schema/comment]]]})
+  #:response
+   {:article        [:map [:article  :schema/article]]
+    :articles       [:map [:articles [:vec :schema/article] [:count :int]]]
+    :user           [:map [:user     :schema/user]]
+    :comment        [:map [:comment  :schema/comment]]
+    :comments       [:map [:comments [:vec :schema/comment]]]
+    :tags           [:map [:tags [:vec :string]]]})
+
+(def ^:private schema
+  (let [{:keys [comment article profile user]} (schemas-from-feilds fields)]
+    #:schema
+     {:user    (-> user    optional-keys closed-schema)
+      :comment (-> comment optional-keys closed-schema)
+      :article (-> article optional-keys closed-schema)
+      :profile (-> profile optional-keys closed-schema)}))
 
 (mr/set-default-registry!
- (merge (m/default-schemas) (mu/schemas) fields schema actions requests responses))
-
-(comment
-  (m/validate
-   :request.user/login
-   {:user {:email "tami5@gmail.com" :password "1234566"}})
-
-  (m/validate
-   :request.user/update
-   {:user {:email "tami5@gmail.com" :password "1234566"}})
-
-  (not (m/validate
-        :request.user/update
-        {:user {:not-field "tami5@gmail.com" :password "1234566"}})))
-
+ (merge (m/default-schemas) (mu/schemas) fields schema (apply merge actions) (apply merge requests) responses))
